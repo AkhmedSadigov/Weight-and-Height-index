f6f42758af3da8b46f5411c6badf58ac237d5d72
ahmed <ahmed@LAPTOP-D0D6I3G9>
2022-10-07 19:11:41 +0400

    Add project files.

---
+++ Weight and Height index/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Weight_and_Height_index
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double height, weight;
+
+            height=Double.Parse(textBox1.Text);
+            weight = Double.Parse(textBox2.Text);
+
+            double result=weight/(height*height);   
+
+            textBox3.Text=result.ToString();
+
+        }
+    }
+}
